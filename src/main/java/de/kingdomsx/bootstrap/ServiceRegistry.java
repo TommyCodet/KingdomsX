3fe1b2416d308ad742c08a5601cb741a803b61a8
TommyCodet <tommycodet247@gmail.com>
2026-06-16 08:32:40 +0200

    Update ServiceRegistry.java

--- src/main/java/de/kingdomsx/bootstrap/ServiceRegistry.java
+++ src/main/java/de/kingdomsx/bootstrap/ServiceRegistry.java
@@ -1,7 +1,29 @@
 package de.kingdomsx.bootstrap;
 
+import de.kingdomsx.kingdom.KingdomManager;
+import de.kingdomsx.kingdom.KingdomService;
+
 public final class ServiceRegistry {
 
-    private ServiceRegistry() {
+    private static KingdomManager kingdomManager;
+    private static KingdomService kingdomService;
+
+    private ServiceRegistry() {}
+
+    public static void initialize() {
+
+        kingdomManager = new KingdomManager();
+
+        kingdomService = new KingdomService(
+                kingdomManager
+        );
+    }
+
+    public static KingdomManager getKingdomManager() {
+        return kingdomManager;
+    }
+
+    public static KingdomService getKingdomService() {
+        return kingdomService;
     }
 }
