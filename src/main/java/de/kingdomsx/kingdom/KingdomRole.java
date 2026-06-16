b9f859cf2f16a19eb40df5168d534f23fc520730
TommyCodet <tommycodet247@gmail.com>
2026-06-16 14:35:15 +0200

    Update KingdomRole.java

--- src/main/java/de/kingdomsx/kingdom/KingdomRole.java
+++ src/main/java/de/kingdomsx/kingdom/KingdomRole.java
@@ -2,11 +2,24 @@ package de.kingdomsx.kingdom;
 
 public enum KingdomRole {
 
-    KING,
-    CO_KING,
-    GENERAL,
-    OFFICER,
-    MEMBER,
-    RECRUIT
+    KING(5),
+    CO_KING(4),
+    GENERAL(3),
+    OFFICER(2),
+    MEMBER(1),
+    RECRUIT(0);
 
+    private final int power;
+
+    KingdomRole(int power) {
+        this.power = power;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public boolean isHigherOrEqual(KingdomRole role) {
+        return this.power >= role.power;
+    }
 }
