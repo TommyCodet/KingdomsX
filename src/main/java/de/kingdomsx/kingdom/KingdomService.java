9b5f1da112eda294d43f79a181329ef4d531ee61
TommyCodet <tommycodet247@gmail.com>
2026-06-16 14:24:49 +0200

    Update KingdomService.java

--- src/main/java/de/kingdomsx/kingdom/KingdomService.java
+++ src/main/java/de/kingdomsx/kingdom/KingdomService.java
@@ -6,19 +6,56 @@ public class KingdomService {
 
     private final KingdomManager kingdomManager;
 
-    public KingdomService(KingdomManager kingdomManager) {
+    public KingdomService(
+            KingdomManager kingdomManager
+    ) {
         this.kingdomManager = kingdomManager;
     }
 
-    public boolean createKingdom(Player player, String name) {
+    public boolean createKingdom(
+            Player player,
+            String name
+    ) {
 
-        if (kingdomManager.hasKingdom(player.getUniqueId())) {
+        if (kingdomManager.hasKingdom(
+                player.getUniqueId()
+        )) {
+            return false;
+        }
+
+        if (name.length() < 3 ||
+                name.length() > 16) {
             return false;
         }
 
         kingdomManager.createKingdom(
                 player.getUniqueId(),
                 name
+        );
+
+        return true;
+    }
+
+    public boolean leaveKingdom(
+            Player player
+    ) {
+
+        Kingdom kingdom =
+                kingdomManager.getKingdomByPlayer(
+                        player.getUniqueId()
+                );
+
+        if (kingdom == null)
+            return false;
+
+        if (kingdom.getOwner()
+                .equals(player.getUniqueId())) {
+            return false;
+        }
+
+        kingdomManager.removeMember(
+                kingdom,
+                player.getUniqueId()
         );
 
         return true;
