449aade5697ecd2ad71690ce2a829e50fe785991
TommyCodet <tommycodet247@gmail.com>
2026-06-16 08:37:36 +0200

    Update StartupManager.java

--- src/main/java/de/kingdomsx/bootstrap/StartupManager.java
+++ src/main/java/de/kingdomsx/bootstrap/StartupManager.java
@@ -15,9 +15,10 @@ public class StartupManager {
         plugin.getLogger().info("Loading configuration...");
         plugin.getLogger().info("Loading services...");
 
+        ServiceRegistry.initialize();
+
         CommandRegistrar.register(plugin);
 
-        
         plugin.getLogger().info("Startup completed.");
     }
 
