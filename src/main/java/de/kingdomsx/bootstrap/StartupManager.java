8e6eca54a0192872f90e00fdee45dfcb4ab22293
TommyCodet <tommycodet247@gmail.com>
2026-06-16 08:23:09 +0200

    Create StartupManager.java

---
+++ src/main/java/de/kingdomsx/bootstrap/StartupManager.java
@@ -0,0 +1,25 @@
+package de.kingdomsx.bootstrap;
+
+import de.kingdomsx.KingdomsX;
+
+public class StartupManager {
+
+    private final KingdomsX plugin;
+
+    public StartupManager(KingdomsX plugin) {
+        this.plugin = plugin;
+    }
+
+    public void start() {
+
+        plugin.getLogger().info("Loading configuration...");
+        plugin.getLogger().info("Loading services...");
+        plugin.getLogger().info("Startup completed.");
+    }
+
+    public void shutdown() {
+
+        plugin.getLogger().info("Saving data...");
+        plugin.getLogger().info("Shutdown completed.");
+    }
+}
